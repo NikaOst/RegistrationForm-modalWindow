7b1a3ad09abbac505a7368778c40b00b3a9926ce
VOstroverkh <veronikaostroverkh@gmail.com>
2025-12-15 12:22:39 +0100

    +validation

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const openModalBtn = document.querySelector('.openModalBtn');
 const modal = document.querySelector('.modal');
 const modalBody = document.querySelector('.modalBody');
 const buttonExt = document.querySelector('#buttonExt');
+const confBtn = document.querySelector('#confBtn');
 const registrationForm = document.querySelector('.registrationForm');
 
 const usernameLabel = document.querySelector('#usernameLabel');
@@ -16,11 +17,11 @@ const passwordInput = document.querySelector('.passwordInput');
 const confPasswordLabel = document.querySelector('#confPasswordLabel');
 const confPasswordInput = document.querySelector('.confPasswordInput');
 
-const firstNameLabel = document.querySelector('#firstNameLabel');
-const firstNameInput = document.querySelector('.firstNameInput');
+// const firstNameLabel = document.querySelector('#firstNameLabel');
+// const firstNameInput = document.querySelector('.firstNameInput');
 
-const lastNameLabel = document.querySelector('#lastNameLabel');
-const lastNameInput = document.querySelector('.lastNameInput');
+// const lastNameLabel = document.querySelector('#lastNameLabel');
+// const lastNameInput = document.querySelector('.lastNameInput');
 
 const ageUserLabel = document.querySelector('#ageUserLabel');
 const ageUserInput = document.querySelector('.ageUserInput');
@@ -35,31 +36,129 @@ function renderStatusMessage(parent, data) {
   statusMessageElement.style.color = data.color;
 }
 
-// function usernameValidation(value) {
-//   return value.length >= 2 && value.length <= 26
-//     ? {
-//         text: 'user name is correct',
-//         color: 'green',
-//       }
-//     : {
-//         text: 'user name is invalid',
-//         color: 'red',
-//       };
-// }
+async function getUsers() {
+  try {
+    const data = await fetch('https://dummyjson.com/users', { method: 'GET' });
+    if (!data) {
+      throw new Error('Data not found');
+    }
+    return await data.json();
+  } catch (error) {
+    return error;
+  }
+}
 
+// validation
+function checkOnSymbol(value) {
+  const symbols = ['-', '_', '*', '+', '$', '#', '^', '@'];
+  let i = 0;
+  while (i < symbols.length) {
+    if (value.includes(symbols[i])) {
+      return true;
+    }
+    ++i;
+  }
+  return false;
+}
+
+async function usernameValidation(value) {
+  // для проверки - emilys уже существует
+  const data = await getUsers();
+  if (
+    data.users.find((user) => {
+      return user.username === value;
+    }) !== undefined
+  ) {
+    return {
+      text: 'username is already taken',
+      color: 'red',
+    };
+  } else {
+    return {
+      text: 'username is correct',
+      color: 'green',
+    };
+  }
+}
+
+function passwordValidation(value) {
+  const ckeckSymbol = checkOnSymbol(value);
+  return value.length >= 6 && ckeckSymbol
+    ? {
+        text: 'password is correct',
+        color: 'green',
+      }
+    : {
+        text: 'password is invalid',
+        color: 'red',
+      };
+}
+
+function passwordsEqual(pass, confPass) {
+  if (pass !== confPass) {
+    return { text: 'password is invalid', color: 'red' };
+  } else return { text: 'Пароли должны совпадать', color: 'green' };
+}
+
+function emailValidation(value) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(value)
+    ? {
+        text: 'email is correct',
+        color: 'green',
+      }
+    : {
+        text: 'email is invalid',
+        color: 'red',
+      };
+}
+
+function ageValidation(value) {
+  return Number(value) >= 10 && Number(value) <= 100
+    ? {
+        text: 'age is correct',
+        color: 'green',
+      }
+    : {
+        text: 'age is invalid',
+        color: 'red',
+      };
+}
+
+// modal window
 const toggleModalWindow = () => {
   modal.classList.toggle('modalHidden');
 };
 openModalBtn.addEventListener('click', toggleModalWindow);
 modal.addEventListener('click', toggleModalWindow);
 modalBody.addEventListener('click', (e) => e.stopPropagation());
-// button
 buttonExt.addEventListener('click', toggleModalWindow);
-// esc
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape') {
     modal.classList.add('modalHidden');
   }
 });
-
 registrationForm.addEventListener('reset', toggleModalWindow);
+
+// input events
+emailInput.addEventListener('input', (event) => {
+  const statusData = emailValidation(event.target.value);
+  renderStatusMessage(emailLabel, statusData);
+});
+passwordInput.addEventListener('input', (event) => {
+  const statusData = passwordValidation(event.target.value);
+  renderStatusMessage(passwordLabel, statusData);
+});
+confPasswordInput.addEventListener('input', (event) => {
+  const statusData = passwordsEqual(event.target.value, passwordInput.value);
+  renderStatusMessage(confPasswordLabel, statusData);
+});
+ageUserInput.addEventListener('input', (event) => {
+  const statusData = ageValidation(event.target.value);
+  renderStatusMessage(ageUserLabel, statusData);
+});
+usernameInput.addEventListener('input', async (event) => {
+  const statusData = await usernameValidation(event.target.value);
+  console.log(statusData);
+  renderStatusMessage(usernameLabel, statusData);
+});
